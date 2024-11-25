4ab2c022536c46b1e8e1c4cba1b7ce77188faee9
JMarcelox12 <jm.costasevero12@gmail.com>
2024-11-25 11:36:06 -0300

    Adição do Firebase como back-end do nosso projeto

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyChEWlgobOb5wfTulPHdVsd77TZTrZRBUo",
+  authDomain: "projeto-de-tcc-50c46.firebaseapp.com",
+  projectId: "projeto-de-tcc-50c46",
+  storageBucket: "projeto-de-tcc-50c46.firebasestorage.app",
+  messagingSenderId: "993919445518",
+  appId: "1:993919445518:web:cd17568a52d51f88ebea5f"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
